7ac095e1f71dc0663cc0fecb40cb1cc03a6950a2
Your Name <sheharaashen535@gmail.com>
2019-06-17 16:32:59 +0530

    PHP test

---
+++ Assignment-01/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ashen Shehara
+ * Date: 6/17/2019
+ * Time: 4:18 PM
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Asheen</h1>
+</body>
+</html>
